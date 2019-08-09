bf557f829d7213124873966ef4ab75a1a68bf379
zhoujf <zhoujf1636@163.com>
2019-08-09 15:40:48 +0800

    Initial Commit

---
+++ virtual/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  virtual
+//
+//  Created by JF Zhou on 2019/8/9.
+//  Copyright © 2019 JF Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
